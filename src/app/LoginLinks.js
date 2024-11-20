96cc998ddd6b3592eae4368d4045d801f730b9d1
Sherwin Christopher Roxas <sherwin.roxas@neti.local>
2024-11-20 15:00:33 +0800

    finished setting up home page

--- src/app/LoginLinks.js
+++ src/app/LoginLinks.js
@@ -8,7 +8,7 @@ const LoginLinks = () => {
   const { user } = useAuth({ middleware: "guest" });
 
   return (
-    <div className="flex justify-end items-end">
+    <>
       {user ? (
         <div className="flex p-4">
           <motion.div
@@ -16,29 +16,49 @@ const LoginLinks = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
           >
-            <Link
-              href="/dashboard"
-              className="bg-stone-800 text-slate-50 
-              rounded-full text-lg py-3 px-8"
+            <motion.button
+              whileHover={{ scale: 1.2 }}
+              onHoverStart={(event) => {}}
+              onHoverEnd={(event) => {}}
+              whileTap={{ scale: 0.9, rotate: 3 }}
             >
-              Dashboard
-            </Link>
+              <Link
+                href="/dashboard"
+                className="bg-stone-950 hover:bg-purple-700 text-slate-50 
+                rounded-full 
+                text-sm md:text-lg lg:text-lg 
+                py-2 md:py-3 lg:py-3 
+                px-5 md:px-8 lg:px-8"
+              >
+                Dashboard
+              </Link>
+            </motion.button>
           </motion.div>
         </div>
       ) : (
-        <div className="flex flex-row gap-2 p-4">
+        <div className="flex flex-col gap-8 p-4">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.2 }}
           >
-            <Link
-              href="/login"
-              className="bg-stone-800 text-slate-50 
-              rounded-full text-lg py-3 px-8"
+            <motion.button
+              whileHover={{ scale: 1.2 }}
+              onHoverStart={(event) => {}}
+              onHoverEnd={(event) => {}}
+              whileTap={{ scale: 0.9, rotate: 3 }}
             >
-              Login
-            </Link>
+              <Link
+                href="/login"
+                className="bg-stone-950 hover:bg-purple-700 text-slate-50 
+                rounded-full 
+                text-sm md:text-lg lg:text-lg 
+                py-2 md:py-3 lg:py-3 
+                px-5 md:px-8 lg:px-8"
+              >
+                Login
+              </Link>
+            </motion.button>
           </motion.div>
 
           <motion.div
@@ -46,17 +66,27 @@ const LoginLinks = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.4 }}
           >
-            <Link
-              href="/register"
-              className="bg-stone-800 text-slate-50 
-              rounded-full text-lg py-3 px-8"
+            <motion.div
+              whileHover={{
+                scale: 1.05,
+                color: "#2563EB",
+                textDecoration: "underline",
+              }}
+              whileTap={{ scale: 0.95 }}
             >
-              Register
-            </Link>
+              <Link
+                href="/register"
+                className="text-stone-950 font-semibold 
+                text-sm md:text-lg lg:text-lg 
+                py-1 md:py-3 lg:py-3"
+              >
+                Don't have an account? Click here to get started!
+              </Link>
+            </motion.div>
           </motion.div>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import WelcomeLabel from "@/components/home-page/WelcomeLabel";
 import NykJvAnimatedLogo from "@/components/home-page/NykJvAnimatedLogo";
+import Footer from "@/components/home-page/Footer";
+import SrmLogo from "@/components/home-page/SrmLogo";
 
 export const metadata = {
   title: "Laravel",
@@ -8,11 +10,14 @@ export const metadata = {
 const Home = () => {
   return (
     <>
-      <div className="flex flex-col bg-slate-50 min-h-screen">
-        <div className="flex flex-col lg:flex-row justify-center items-center gap-8 p-4">
+      <div className="flex flex-col custom-bg-nyk min-h-screen">
+        <SrmLogo />
+        <div className="flex flex-col lg:flex-row justify-center items-center gap-8 p-2">
           <WelcomeLabel />
+
           <NykJvAnimatedLogo />
         </div>
+        <Footer />
       </div>
     </>
   );
