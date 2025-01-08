fa17ffeaa8d4ff65ef498d3bc395bd1ca5b85dfc
Yujin <127990921+patrickeugenesacueza@users.noreply.github.com>
2025-01-08 14:45:17 +0800

    Update: TotalSupplier.js and TotalProducts.js
    Add skeleton loading in total count container

--- src/components/dashboard/TotalProduct.js
+++ src/components/dashboard/TotalProduct.js
@@ -3,41 +3,55 @@
 import React from "react";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 import axios from "@/lib/axios";
+import Skeleton from "@mui/material/Skeleton";
+import Box from "@mui/material/Box";
 
 const TotalProduct = () => {
-    const [totalProduct, setTotalProduct ] = React.useState(0);
+  const [totalProduct, setTotalProduct] = React.useState(0);
+  const [loading, setLoading] = React.useState(true);
 
-    const fetchTotalProduct = async () => {
-      try{
-        const response = await axios.get('/api/products/total_count');
-        setTotalProduct(response.data.total || 0);
-        console.log('Total product count:', response.data.total);
-      }
-      catch(error){
-        console.log('Error fetching total count of product', error);
-      }
+  const fetchTotalProduct = async () => {
+    try {
+      const response = await axios.get("/api/products/total_count");
+      setTotalProduct(response.data.total || 0);
+      console.log("Total product count:", response.data.total);
+      setLoading(false);
+    } catch (error) {
+      console.log("Error fetching total count of product", error);
     }
+  };
 
-    React.useEffect(() => {
-      fetchTotalProduct();
-    }, []);
+  React.useEffect(() => {
+    fetchTotalProduct();
+  }, []);
 
-     return (
-        <>
-          <div className="">
-            <div className="p-4 text-center">
-              <div className="font-semibold text-lg mb-4">Total Product</div>
-              <div className="font-bold lg:text-5xl xl:text-7xl mb-4">{totalProduct}</div>
-              <div className="flex text-center items-center justify-center gap-2">
-                <div className="">
-                  <TrendingUpIcon sx={{textColor: '#36bf5a'}}/>
-                </div>
-                <div className="lg:text-[10px] xl:text-xl">24% from Etibac</div>
+  return (
+    <>
+      {loading ? (
+        <Box sx={{ width: "100%", p: 4 }}>
+          <Skeleton />
+          <Skeleton animation="wave" />
+          <Skeleton animation={false} sx={{ marginTop: 2 }} />
+          <Skeleton animation={false} sx={{ marginTop: 2 }} />
+        </Box>
+      ) : (
+        <div className="">
+          <div className="p-4 text-center">
+            <div className="font-semibold text-lg mb-4">Total Product</div>
+            <div className="font-bold lg:text-5xl xl:text-7xl mb-4">
+              {totalProduct}
+            </div>
+            <div className="flex text-center items-center justify-center gap-2">
+              <div className="">
+                <TrendingUpIcon sx={{ textColor: "#36bf5a" }} />
               </div>
+              <div className="lg:text-[10px] xl:text-xl">24% from Etibac</div>
             </div>
           </div>
-        </>
-      );
-}
+        </div>
+      )}
+    </>
+  );
+};
 
 export default TotalProduct;
--- src/components/dashboard/TotalSupplier.js
+++ src/components/dashboard/TotalSupplier.js
@@ -3,35 +3,49 @@
 import React from "react";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 import axios from "@/lib/axios";
-
+import Skeleton from "@mui/material/Skeleton";
+import Box from "@mui/material/Box";
 const TotalSupplier = () => {
-  const [totalSupplier, setTotalSupplier] = React.useState(0); 
+  const [totalSupplier, setTotalSupplier] = React.useState(0);
+  const [loading, setLoading] = React.useState(true);
 
   const fetchTotalSupplier = async () => {
     try {
-        console.log("Fetching total supplier count...");
-        const response = await axios.get("/api/supplier/total_count");
-        console.log("Response:", response);
-        setTotalSupplier(response.data.total || 0);
+      console.log("Fetching total supplier count...");
+      const response = await axios.get("/api/supplier/total_count");
+      console.log("Response:", response);
+      setTotalSupplier(response.data.total || 0);
+      setLoading(false);
     } catch (error) {
-        console.error("Failed to fetch total suppliers:", error);
+      console.error("Failed to fetch total suppliers:", error);
     }
-};
+  };
   React.useEffect(() => {
     fetchTotalSupplier();
   }, []);
 
   return (
-    <div className="p-4 text-center">
-      <div className="font-semibold text-lg mb-4">Total Supplier</div>
-      <div className="font-bold lg:text-5xl xl:text-7xl mb-4">
-        {totalSupplier} {/* Display totalSupplier count */}
-      </div>
-      <div className="flex text-center items-center justify-center gap-2">
-        <TrendingUpIcon sx={{ color: "#36bf5a" }} />
-        <div className="lg:text-[10px] xl:text-xl">24% from Etibac</div>
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <Box sx={{ width: "100%", p: 4 }}>
+          <Skeleton />
+          <Skeleton animation="wave" />
+          <Skeleton animation={false} sx={{marginTop: 2}} />
+          <Skeleton animation={false} sx={{marginTop: 2}} />
+        </Box>
+      ) : (
+        <div className="p-4 text-center">
+          <div className="font-semibold text-lg mb-4">Total Supplier</div>
+          <div className="font-bold lg:text-5xl xl:text-7xl mb-4">
+            {totalSupplier} {/* Display totalSupplier count */}
+          </div>
+          <div className="flex text-center items-center justify-center gap-2">
+            <TrendingUpIcon sx={{ color: "#36bf5a" }} />
+            <div className="lg:text-[10px] xl:text-xl">24% from Etibac</div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
